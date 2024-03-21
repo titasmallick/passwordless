827768461bd6290476fb8298a1f2d0828e063ad4
TITAS MALLICK <44458514+titasmallick@users.noreply.github.com>
2024-03-21 10:15:30 +0530

    modify

--- app/protected/page.jsx
+++ app/protected/page.jsx
@@ -1,8 +1,7 @@
 const Protected = () => {
     return ( 
         <main>
-            <h1>This is a protected route</h1>
-            
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">This is a protected route</h1>
         </main>
      );
 }
