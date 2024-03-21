d6dd92401b9de84331a14b7c072db63d40f33423
TITAS MALLICK <44458514+titasmallick@users.noreply.github.com>
2024-03-21 09:34:30 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import GenerateLink from "@/components/generateLink";
+import { Suspense } from "react";
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+			<Suspense>
 			<GenerateLink />
+			</Suspense>
 		</section>
 	);
 }
